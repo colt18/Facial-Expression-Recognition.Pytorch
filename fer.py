49b42e1fffdee3e6d6e85aed0ed5d74c308719df
Ömer Faruk SÖYLEMEZ <omer_soylemez@hotmail.com>
2020-04-04 01:06:50 +0300

    Update fer.py

--- fer.py
+++ fer.py
@@ -7,46 +7,33 @@ import h5py
 import torch.utils.data as data
 
 class FER2013(data.Dataset):
-    """`FER2013 Dataset.
-
-    Args:
-        train (bool, optional): If True, creates dataset from training set, otherwise
-            creates from test set.
-        transform (callable, optional): A function/transform that  takes in an PIL image
-            and returns a transformed version. E.g, ``transforms.RandomCrop``
-    """
-
+ 
     def __init__(self, split='Training', transform=None):
         self.transform = transform
         self.split = split  # training set or test set
-        self.data = h5py.File('./data/data.h5', 'r', driver='core')
+        # self.data = h5py.File('./data/data.h5', 'r', driver='core')
+
+        
         # now load the picked numpy arrays
         if self.split == 'Training':
-            self.train_data = self.data['Training_pixel']
-            self.train_labels = self.data['Training_label']
-            self.train_data = np.asarray(self.train_data)
+            self.train_data = np.load('data/Training_x.npy')
+            self.train_labels = np.load('data/Training_y.npy')
+            # self.train_data = np.asarray(self.train_data)
             self.train_data = self.train_data.reshape((28709, 48, 48))
-
         elif self.split == 'PublicTest':
-            self.PublicTest_data = self.data['PublicTest_pixel']
-            self.PublicTest_labels = self.data['PublicTest_label']
-            self.PublicTest_data = np.asarray(self.PublicTest_data)
+            self.PublicTest_data = np.load('data/PublicTest_x.npy')
+            self.PublicTest_labels = np.load('data/PublicTest_y.npy')
+            # self.PublicTest_data = np.asarray(self.PublicTest_data)
             self.PublicTest_data = self.PublicTest_data.reshape((3589, 48, 48))
 
         else:
-            self.PrivateTest_data = self.data['PrivateTest_pixel']
-            self.PrivateTest_labels = self.data['PrivateTest_label']
-            self.PrivateTest_data = np.asarray(self.PrivateTest_data)
+            self.PrivateTest_data = np.load('data/PrivateTest_x.npy')
+            self.PrivateTest_labels = np.load('data/PrivateTest_y.npy')
+            # self.PrivateTest_data = np.asarray(self.PrivateTest_data)
             self.PrivateTest_data = self.PrivateTest_data.reshape((3589, 48, 48))
 
     def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: (image, target) where target is index of the target class.
-        """
+      
         if self.split == 'Training':
             img, target = self.train_data[index], self.train_labels[index]
         elif self.split == 'PublicTest':
@@ -56,11 +43,11 @@ class FER2013(data.Dataset):
 
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        img = img[:, :, np.newaxis]
-        img = np.concatenate((img, img, img), axis=2)
-        img = Image.fromarray(img)
-        if self.transform is not None:
-            img = self.transform(img)
+        # img = img[:, :, np.newaxis]
+        # img = np.concatenate((img, img, img), axis=2)
+        # img = Image.fromarray(img)
+        # if self.transform is not None:
+        #     img = self.transform(img)
         return img, target
 
     def __len__(self):
